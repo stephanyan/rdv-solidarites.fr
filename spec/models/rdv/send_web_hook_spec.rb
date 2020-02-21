9636d24b5c90b5460b1a99fa4c169f82e7da6594
Thomas Guillet <guillet.thomas@gmail.com>
2020-02-21 16:42:53 +0100

    Teste le format des datetimes

--- spec/models/rdv/send_web_hook_spec.rb
+++ spec/models/rdv/send_web_hook_spec.rb
@@ -6,11 +6,20 @@ describe Rdv, type: :model do
   end
 
   describe '#send_web_hook' do
-    let(:rdv) { create(:rdv) }
+    let(:rdv) { create(:rdv, starts_at: "2020-02-17T10:20") }
+
+    RSpec::Matchers.define :date_in_iso_format do
+      match { |actual| (actual.to_json.match?(/"starts_at"\:"2020-02-17T10:20:00.000\+01\:00"/)) }
+    end
 
     describe 'default publication' do
       it 'publishes the default status' do
         expect(WebHookJob).to receive(:perform_later).with(hash_including(status: 'unknown'))
+        rdv.reload
+      end
+
+      it 'publishes will convert to valid ISO dates' do
+        expect(WebHookJob).to receive(:perform_later).with(date_in_iso_format)
         rdv.reload
       end
     end
