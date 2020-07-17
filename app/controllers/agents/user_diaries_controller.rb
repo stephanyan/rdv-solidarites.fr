4506270a18a22d82c160c1d866a58c791ac3d740
Yannick Francois <yaf@elsif.fr>
2020-07-17 13:53:24 +0200

    enregistre les notes

--- app/controllers/agents/user_diaries_controller.rb
+++ app/controllers/agents/user_diaries_controller.rb
@@ -3,6 +3,6 @@ class Agents::UserDiariesController < AgentAuthController
     user = User.find(params[:user_id])
     authorize(user)
     UserDiary.create!(organisation: current_organisation, user: user, agent: current_agent, text: params[:user_diary][:text])
-    redirect_to organisation_user_path(current_organisation, user)
+    redirect_back(fallback_location: organisation_user_path(current_organisation, user))
   end
 end
--- spec/controllers/agents/user_diaries_controller_spec.rb
+++ spec/controllers/agents/user_diaries_controller_spec.rb
@@ -2,7 +2,7 @@ describe Agents::UserDiariesController, type: :controller do
   render_views
 
   describe "#create" do
-    it "works" do
+    it "from user page, create a diary and redirect to user page" do
       agent = create(:agent)
       organisation = agent.organisations.first
       user = create(:user, organisations: [organisation])
@@ -11,6 +11,21 @@ describe Agents::UserDiariesController, type: :controller do
         post :create, params: { organisation_id: organisation.id, user_id: user.id, "user_diary" => { "text" => "un truc nouveau à ajouter" } }
       end.to change(UserDiary, :count).by(1)
       expect(response).to redirect_to(organisation_user_path(organisation, user))
+    end
+
+    it "from rdv page, create a diary for each users of rdv, and redirect to rdv page" do
+      organisation = create(:organisation)
+      user = create(:user, organisations: [organisation])
+      enfant = create(:user, :relative, responsible: user)
+      rdv = create(:rdv, :future, users: [user, enfant], organisation: organisation)
+      agent = rdv.agents.first
+      sign_in agent
+
+      request.headers["Referer"] = organisation_rdv_path(organisation, rdv)
+      expect do
+        post :create, params: { organisation_id: organisation.id, user_id: user.id, "user_diary" => { "user_ids" => enfant.id.to_s, "text" => "un truc nouveau à ajouter" } }
+      end.to change(UserDiary, :count).by(1)
+      expect(response).to redirect_to(organisation_rdv_path(organisation, rdv))
     end
   end
 end
