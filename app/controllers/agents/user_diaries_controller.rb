bc104cf6dd5c94ea7fe2fa45c5ef1926acbafa59
Yannick Francois <yaf@elsif.fr>
2020-07-30 17:26:32 +0200

    repositionne sur le bloc du journal après l'écriture d'une note

--- app/controllers/agents/user_diaries_controller.rb
+++ app/controllers/agents/user_diaries_controller.rb
@@ -3,6 +3,10 @@ class Agents::UserDiariesController < AgentAuthController
     user = User.find(params[:user_id])
     authorize(user)
     UserDiary.create!(organisation: current_organisation, user: user, agent: current_agent, text: params[:user_diary][:text])
-    redirect_back(fallback_location: organisation_user_path(current_organisation, user))
+    if request.headers["HTTP_REFERER"]
+      redirect_to(request.headers["HTTP_REFERER"] += '#notes')
+    else
+      redirect_to organisation_user_path(current_organisation, user, anchor: "notes")
+    end
   end
 end
--- spec/controllers/agents/user_diaries_controller_spec.rb
+++ spec/controllers/agents/user_diaries_controller_spec.rb
@@ -10,7 +10,7 @@ describe Agents::UserDiariesController, type: :controller do
       expect  do
         post :create, params: { organisation_id: organisation.id, user_id: user.id, "user_diary" => { "text" => "un truc nouveau à ajouter" } }
       end.to change(UserDiary, :count).by(1)
-      expect(response).to redirect_to(organisation_user_path(organisation, user))
+      expect(response).to redirect_to(organisation_user_path(organisation, user, anchor: "notes"))
     end
 
     it "from rdv page, create a diary for each users of rdv, and redirect to rdv page" do
@@ -25,7 +25,7 @@ describe Agents::UserDiariesController, type: :controller do
       expect do
         post :create, params: { organisation_id: organisation.id, user_id: user.id, "user_diary" => { "user_ids" => enfant.id.to_s, "text" => "un truc nouveau à ajouter" } }
       end.to change(UserDiary, :count).by(1)
-      expect(response).to redirect_to(organisation_rdv_path(organisation, rdv))
+      expect(response).to redirect_to(organisation_rdv_path(organisation, rdv, anchor: "notes"))
     end
   end
 end
